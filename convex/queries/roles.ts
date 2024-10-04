3029d26534d93015880e17fe9c945e3966b59c86
Sean Gray <seangray.dev@gmail.com>
2024-10-04 02:58:40 -0400

    feat: add role query for fetching by ID and enhance theme queries for thtmes retrieval based on dream ID

--- convex/queries/roles.ts
+++ convex/queries/roles.ts
@@ -1,7 +1,16 @@
+import { v } from "convex/values";
+
 import { query } from "../_generated/server";
 
 export const getAllRoles = query({
   handler: async (ctx) => {
     return await ctx.db.query("roles").collect();
+  },
+});
+
+export const getRoleById = query({
+  args: { id: v.id("roles") },
+  handler: async (ctx, args) => {
+    return await ctx.db.get(args.id);
   },
 });
--- convex/queries/themes.ts
+++ convex/queries/themes.ts
@@ -1,7 +1,27 @@
+import { v } from "convex/values";
+
 import { query } from "../_generated/server";
 
 export const getAllThemes = query({
   handler: async (ctx) => {
-    return await ctx.db.query("themes").collect();
+    const themes = await ctx.db.query("themes").collect();
+    return themes;
+  },
+});
+
+export const getAllThemesToDream = query({
+  args: { dreamId: v.id("dreams") },
+  handler: async (ctx, args) => {
+    const { dreamId } = args;
+
+    const dream = await ctx.db.get(dreamId);
+
+    const themeIds = dream?.themes ?? [];
+
+    const themes = await Promise.all(
+      themeIds.map((themeId) => ctx.db.get(themeId))
+    );
+
+    return themes;
   },
 });
