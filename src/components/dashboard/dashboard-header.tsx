bfd09854484acd7a6dbfe2cc8ca11affbfc25eb6
Sean Gray <seangray.dev@gmail.com>
2024-10-07 09:18:18 -0400

    feat: enhance DashboardHeader with loading state and remove unused Loader and Spinner components

--- src/components/dashboard/dashboard-header.tsx
+++ src/components/dashboard/dashboard-header.tsx
@@ -3,15 +3,31 @@
 import { useSession } from "@/lib/client-auth";
 import getGreeting from "@/utils/get-greeting";
 
+import Loader from "../shared/loader";
 import { AddDreamButton } from "./add-dream-button";
 
 export default function DashboardHeader() {
-  const session = useSession();
-  const user = session?.session?.user;
+  const { session, isLoaded, isLoggedIn } = useSession();
+
+  if (!isLoggedIn) {
+    return null;
+  }
+
+  if (!isLoaded) {
+    return <Loader />;
+  }
+
+  if (!session) {
+    return null;
+  }
+
+  const user = session?.user;
 
   return (
     <section className="sm:flew-row container flex flex-wrap items-center justify-between gap-4">
-      <h1 className="text-xl font-medium sm:text-2xl">{`${getGreeting()}, ${user?.firstName || "User"}!`}</h1>
+      {isLoaded && (
+        <h1 className="text-xl font-medium sm:text-2xl">{`${getGreeting()}, ${user?.firstName || "User"}!`}</h1>
+      )}
       <AddDreamButton />
     </section>
   );
