19b5edd7d5162497c2f18b5d726592b5d89af9aa
Sean Gray <seangray.dev@gmail.com>
2024-10-10 05:04:04 -0400

    fix: adjust padding in DreamPage and Tabs components

--- src/components/layout/tabs/index.tsx
+++ src/components/layout/tabs/index.tsx
@@ -21,7 +21,7 @@ export default function Tabs() {
   }
 
   return (
-    <div className="fixed bottom-0 left-0 w-full border-t bg-background pb-6 shadow-[0_-2px_4px_rgba(0,0,0,0.1)] sm:hidden">
+    <div className="fixed bottom-0 left-0 w-full border-t bg-background pb-5 shadow-[0_-2px_4px_rgba(0,0,0,0.1)] sm:hidden">
       <nav className="flex h-16 items-center justify-around">
         {/* Render the first two items */}
         {navItems.slice(0, 2).map(({ href, label, Icon }) => {
