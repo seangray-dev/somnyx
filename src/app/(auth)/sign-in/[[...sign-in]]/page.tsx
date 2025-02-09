4128e487495f199fc08d48ebb717a277025e6c61
Sean Gray <seangray.dev@gmail.com>
2025-02-09 17:19:26 -0500

    feat: Add dark theme support for Clerk authentication pages

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,19 @@
+"use client";
+
 import { SignIn } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import { useTheme } from "next-themes";
 
 export default function Page() {
+  const { theme } = useTheme();
+
   return (
-    <div className="flex h-screen items-center justify-center">
-      <SignIn />
+    <div className="flex flex-1 flex-col items-center justify-center">
+      {theme === "dark" ? (
+        <SignIn appearance={{ baseTheme: dark }} />
+      ) : (
+        <SignIn />
+      )}
     </div>
   );
 }
