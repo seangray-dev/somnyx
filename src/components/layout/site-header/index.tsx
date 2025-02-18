33c4904fc343c9cb3286c5e5ea1ddd7c02b52349
Sean Gray <seangray.dev@gmail.com>
2025-02-17 19:43:34 -0500

    refactor: enhance theme toggle with animated icons and simplified markup

--- src/components/layout/site-header/index.tsx
+++ src/components/layout/site-header/index.tsx
@@ -67,9 +67,7 @@ export default function SiteHeader() {
             </Link>
           </div>
         </div>
-        <div className="flex items-center gap-4">
-          <HeaderActions />
-        </div>
+        <HeaderActions />
       </nav>
     </header>
   );
--- src/components/shared/theme-toggle.tsx
+++ src/components/shared/theme-toggle.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
@@ -11,15 +10,18 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
+import { MoonIcon } from "../ui/icons/moon";
+import { SunIcon } from "../ui/icons/sun";
+
 export function ThemeToggle() {
   const { setTheme } = useTheme();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button className="bg-transparent" variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <Button variant="ghost" size="icon">
+          <SunIcon className="dark:hidden" />
+          <MoonIcon className="hidden dark:block" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
