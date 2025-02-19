2c3a3a60179e3d7a9ce1da4641aac7e1982217f7
Sean Gray <seangray.dev@gmail.com>
2025-02-19 18:26:28 -0500

    feat: add PWA icons and update manifest configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,8 +46,8 @@ export const metadata: Metadata = {
   manifest: "/manifest.json",
   icons: {
     icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
+    shortcut: "/favicon.ico",
+    apple: "/images/apple-touch-icon.png",
   },
 };
 
--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -13,9 +13,14 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#030712",
     icons: [
       {
-        src: "./favicon.ico",
-        sizes: "any",
-        type: "image/x-icon",
+        src: "/images/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/images/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
       },
     ],
   };
