5263538a44dce955308937aec965b7bfce7ec37a
Sean Gray <seangray.dev@gmail.com>
2025-02-19 21:23:36 -0500

    fix: remove maskable purpose from PWA icon and update service worker icon paths

--- public/sw.js
+++ public/sw.js
@@ -8,13 +8,13 @@ self.addEventListener("push", function (event) {
       data = {
         title: "Somnyx",
         body: event.data.text(),
-        icon: "/images/icon-192x192.png",
+        icon: "/icon-192x192.png",
       };
     }
 
     const options = {
       body: data.body,
-      icon: data.icon || "/images/icon-192x192.png",
+      icon: data.icon || "/icon-192x192.png",
       vibrate: [100, 50, 100],
       data: {
         dateOfArrival: Date.now(),
