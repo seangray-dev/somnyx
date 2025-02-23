f427f796b3e3fe4427bae30ba9b84ce02d341aae
Sean Gray <seangray.dev@gmail.com>
2025-02-23 07:36:51 -0500

    feat: add back navigation button to dashboard header

--- src/components/layout/tabs/dashboard-header.tsx
+++ src/components/layout/tabs/dashboard-header.tsx
@@ -1,20 +1,22 @@
 "use client";
 
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 import { format } from "date-fns";
+import { ChevronLeftIcon } from "lucide-react";
 
+import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
+import PushNotificationManager from "@/features/notifications/components/push-notification-manager";
 import { useSession } from "@/lib/client-auth";
 import getGreeting from "@/utils/get-greeting";
 
 import UserDropdownMenu from "../site-header/user-dropdown-menu";
-import PushNotificationManager from "@/features/notifications/components/push-notification-manager";
 
 export default function DashboardHeader() {
   const { session, isLoaded, isLoggedIn } = useSession();
   const pathname = usePathname();
-
+  const router = useRouter();
   if (pathname === "/settings" || (!isLoggedIn && isLoaded)) {
     return null;
   }
@@ -23,7 +25,18 @@ export default function DashboardHeader() {
   const firstName = user?.firstName;
 
   return (
-    <section className="flex flex-col gap-4 border-b py-5">
+    <section className="flex flex-col gap-4 border-b pb-5">
+      <div className="container pt-1">
+        <Button
+          onClick={() => router.back()}
+          size="sm"
+          variant="ghost"
+          className="pl-0"
+        >
+          <ChevronLeftIcon className="size-4" />
+          Back
+        </Button>
+      </div>
       <div className="container flex flex-wrap items-center justify-between gap-4">
         <div className="flex flex-col gap-1">
           <p className="text-xs text-muted-foreground">
