cefcba90e469748b67c4b649d0c0f553c34ced19
Sean Gray <seangray.dev@gmail.com>
2025-03-02 12:34:04 -0500

    feat: setup reverse proxy for posthog

---
+++ src/providers/pageview-tracker.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { usePathname, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
+import { useUser } from "@clerk/nextjs";
+import { usePostHog } from "posthog-js/react";
+
+export default function PostHogPageView(): null {
+  const posthog = usePostHog();
+  const userInfo = useUser();
+  const { user } = userInfo;
+
+  useEffect(() => {
+    if (user?.id) {
+      posthog.identify(user.id, {
+        email: user.emailAddresses[0]?.emailAddress,
+      });
+    } else {
+      posthog.reset();
+    }
+  }, [posthog, user]);
+
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  // Track pageviews
+  useEffect(() => {
+    if (pathname && posthog) {
+      let url = window.origin + pathname;
+      if (searchParams.toString()) {
+        url = url + `?${searchParams.toString()}`;
+      }
+
+      posthog.capture("$pageview", { $current_url: url });
+    }
+  }, [pathname, searchParams, posthog]);
+
+  return null;
+}
--- src/providers/posthog-provider.tsx
+++ src/providers/posthog-provider.tsx
@@ -1,20 +1,25 @@
-// app/providers.tsx
 "use client";
 
-import { usePathname, useSearchParams } from "next/navigation";
-import { Suspense, useEffect } from "react";
+import dynamicLoader from "next/dynamic";
+import { useEffect } from "react";
 
 import posthog from "posthog-js";
-import { PostHogProvider as PHProvider, usePostHog } from "posthog-js/react";
+import { PostHogProvider as PHProvider } from "posthog-js/react";
 
-// app/providers.tsx
+import { env } from "@/config/env/client";
+
+const SuspendedPostHogPageView = dynamicLoader(
+  () => import("./pageview-tracker"),
+  {
+    ssr: false,
+  }
+);
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-      api_host:
-        process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
-      person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+    posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
+      api_host: "/ingest",
+      ui_host: "https://us.posthog.com",
       capture_pageview: false, // Disable automatic pageview capture, as we capture manually
     });
   }, []);
@@ -24,36 +29,5 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
       <SuspendedPostHogPageView />
       {children}
     </PHProvider>
-  );
-}
-
-function PostHogPageView() {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-  const posthog = usePostHog();
-
-  // Track pageviews
-  useEffect(() => {
-    if (pathname && posthog) {
-      let url = window.origin + pathname;
-      if (searchParams.toString()) {
-        url = url + "?" + searchParams.toString();
-      }
-
-      posthog.capture("$pageview", { $current_url: url });
-    }
-  }, [pathname, searchParams, posthog]);
-
-  return null;
-}
-
-// Wrap PostHogPageView in Suspense to avoid the useSearchParams usage above
-// from de-opting the whole app into client-side rendering
-// See: https://nextjs.org/docs/messages/deopted-into-client-rendering
-function SuspendedPostHogPageView() {
-  return (
-    <Suspense fallback={null}>
-      <PostHogPageView />
-    </Suspense>
   );
 }
