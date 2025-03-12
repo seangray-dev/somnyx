e6241009b4d7e04228315ae06a962bd049ae371e
Sean Gray <seangray.dev@gmail.com>
2025-03-12 08:38:25 -0400

    feat(test): import logo images as modules for improved performance

--- src/components/shared/icon-logo.tsx
+++ src/components/shared/icon-logo.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import { applicationName } from "@/config/app";
 import { cn } from "@/lib/utils";
 
+import logoIcon from "../../../public/images/logo-icon-white.png";
+
 export default function IconLogo({ className }: { className?: string }) {
   return (
     <Link
@@ -16,13 +18,12 @@ export default function IconLogo({ className }: { className?: string }) {
     >
       <Image
         className="h-auto w-full invert dark:invert-0"
-        src="/images/logo-icon-white.png"
+        src={logoIcon}
         alt={`${applicationName} Logo`}
         width={32}
         height={32}
         priority
         placeholder="blur"
-        blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAIIAAAAgCAQAAABBQj7cAAAAL0lEQVR42u3BAQ0AAADCoPdPbQ43oAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAODXAEZYAAHyWq6bAAAAAElFTkSuQmCC"
       />
     </Link>
   );
